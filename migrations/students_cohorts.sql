8f6e234d2936214abdbc1c52f490e12698a297dd
Thomas Azran <tommy.azran@gmail.com>
2024-04-04 16:33:01 +0000

    create teacher and assistance requests tables

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,13 +1,13 @@
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
   phone VARCHAR(32),
   github VARCHAR(255),
